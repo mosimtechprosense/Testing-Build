d33580c55161395085983d5ac5736224fcd7ed21
Mosim Raza <mosimraza.techprosense@gmail.com>
2025-12-04 14:46:08 +0530

    Setup the React Redux for global store data management

--- frontend/src/services/listingService.js
+++ frontend/src/services/listingService.js
@@ -1,9 +1,35 @@
 import api from "../utils/axiosInstance";
 
-export const getAllListings = () => api.get("/listings");
+//* Get all listings
+export const getAllListings = () => 
+  api.get("/listings");
 
+
+//* Get listing by ID
 export const getListingById = (id) =>
   api.get(`/listings/${id}`);
 
-export const getFilteredListings = (params) =>
+
+//* Get listings with filters
+export const getFilteredListings = (params = {}) =>
   api.get("/listings", { params });
+
+
+//* Get listings by location
+export const getListingsByLocation = (location, params = {}) =>
+  api.get(`/listings/location/${location}`, { params });
+
+
+//* Create a new listing
+export const createListing = (data) =>
+  api.post("/listings", data);
+
+
+//? Update existing listing
+export const updateListing = (id, data) =>
+  api.put(`/listings/${id}`, data);
+
+
+//! Delete listing
+export const deleteListing = (id) =>
+  api.delete(`/listings/${id}`);
